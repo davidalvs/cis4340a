22c5efd637e71012795b356584edf039a5a413ad
davidalvs <davidalv872@gmail.com>
2025-03-01 18:43:54 -0500

    Update cis4340a.java

--- cis4340a.java
+++ cis4340a.java
@@ -17,7 +17,7 @@ public class cis4340a {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World - from Alex Chavez - Spring 2024 - UCF");
+        System.out.println("Hello, World - from David Alvarado - Spring 2024 - UCF");
     }
 
 }
