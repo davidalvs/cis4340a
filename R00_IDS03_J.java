95532d73a46336cd460ea65e45437083f7a305c8
davidalvs <davidalv872@gmail.com>
2025-03-01 21:27:18 -0500

    Fixed security vulnerability in R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,13 +1,20 @@
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 public class R00_IDS03_J {
     private static final Logger logger = Logger.getLogger(R00_IDS03_J.class.getName());
 
     public static void logUserLogin(boolean loginSuccessful, String username) {
+        username = sanitizeUser(username);
         if (loginSuccessful) {
             logger.severe("User login succeeded for: " + username);
         } else {
             logger.severe("User login failed for: " + username);
         }
     }
+
+    public static String sanitizeUser(String username) {
+        return Pattern.matches("[A-Za-z0-9]+", username) ? username : "unauthorized user";
+    }
 }
+
