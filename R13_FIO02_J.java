75abf00bb407e2bd7aa81c3dc98bbe913c6b2cf1
davidalvs <davidalv872@gmail.com>
2025-03-01 21:39:05 -0500

    Fixed security vulnerability in R13_FIO02_J.java

--- R13_FIO02_J.java
+++ R13_FIO02_J.java
@@ -2,12 +2,12 @@ import java.io.*;
 
 public class R13_FIO02_J {
     public static void readFile(String filename) throws IOException {
-        FileReader fr = new FileReader(filename);
-        BufferedReader br = new BufferedReader(fr);
-        String line;
-        while ((line = br.readLine()) != null) {
-            System.out.println(line);
-        }
-        // FileReader not closed (resource leak)
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } // FileReader and BufferedReader are automatically closed
     }
 }
+
