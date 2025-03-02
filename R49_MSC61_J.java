96e562e4ded01b7c0521d414734781fa064a296c
davidalvs <davidalv872@gmail.com>
2025-03-01 21:24:52 -0500

    Added additional vulnerability R49_MSC61_J.java

---
+++ R49_MSC61_J.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class R49_MSC61_J {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int randomNumber = random.nextInt(); // Unpredictable value
+        System.out.println("Random number: " + randomNumber);
+    }
+}
