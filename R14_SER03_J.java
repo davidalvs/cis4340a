4b7027ca1ff7c3cd2e7868f6c849179723991027
davidalvs <davidalv872@gmail.com>
2025-03-01 21:39:25 -0500

    Fixed security vulnerability in R14_SER03_J.java

--- R14_SER03_J.java
+++ R14_SER03_J.java
@@ -2,10 +2,13 @@ import java.io.*;
 
 public class R14_SER03_J implements Serializable {
     private static final long serialVersionUID = 1L;
-    private String sensitiveData = "123456";
+    private transient String sensitiveData = "123456";
 
     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
         in.defaultReadObject();
-        // No validation of deserialized data
+        if (sensitiveData == null || sensitiveData.length() > 20) {
+            throw new InvalidObjectException("Invalid data detected.");
+        }
     }
 }
+
